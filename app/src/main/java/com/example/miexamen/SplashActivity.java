1e7bd24fb0545c1750e59c1c6ccf276ff001c95f
CristhianNina <44449380+cristhiannina@users.noreply.github.com>
2020-12-17 22:51:15 -0400

    cambiando contadores de la clase categoria editando el Recycle View

--- app/src/main/java/com/example/miexamen/SplashActivity.java
+++ app/src/main/java/com/example/miexamen/SplashActivity.java
@@ -1,57 +1,105 @@
 package com.example.miexamen;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
-
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import static java.lang.Thread.sleep;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SplashActivity extends AppCompatActivity {
 
     private TextView appName;
     private ImageView appImageView;
 
+    public static List<String> catList = new ArrayList<>();
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);// para que la SplasScreen se vea en pantalla completa sin la barra de arriba
         setContentView(R.layout.activity_splash);
 
         appName = findViewById(R.id.appName);
-        Animation anim = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        Typeface typeface = ResourcesCompat.getFont(this,R.font.blacklist);
+        appName.setTypeface(typeface);
+
+        Animation anim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.myanim);
         appName.setAnimation(anim);
 
-        appImageView = findViewById(R.id.appimageView);
-        Animation anim2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
-        appImageView.setAnimation(anim2);
+        firestore = FirebaseFirestore.getInstance();
 
-        new Thread(new Runnable() {
-            @Override
+        new Thread() {
             public void run() {
 
-                try {
-                    sleep(2000);
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                    //sleep(3000);
+                    loadData();
 
             }
-        }).start();
+        }.start();
 
     }
+
+    private void loadData()    {
+        catList.clear();
+
+        firestore.collection("QUIZ").document("Categories")
+                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                if(task.isSuccessful())
+                {
+                    DocumentSnapshot doc = task.getResult();
+
+                    if(doc.exists())
+                    {
+                        long count = (long)doc.get("COUNT");
+
+                        for(int i=1; i <= count; i++)
+                        {
+                            String catName = doc.getString("CAT" + String.valueOf(i));
+
+                            catList.add(catName);
+                        }
+
+
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        SplashActivity.this.finish();
+
+                    }
+                    else
+                    {
+                        Toast.makeText(SplashActivity.this,"No Category Document Exists!",Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+
+                }
+                else
+                {
+
+                    Toast.makeText(SplashActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
 }
+
